b86e1afc76293f28bd619a5fc13e2c89b262581b
Maxim Dankovtsev <max.dankow@gmail.com>
2015-03-25 16:53:55 +0300

    All good.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <algorithm>
 
+typedef std::vector<std::vector<std::pair<long long,long long> > > Way;
+static const long long WAY_END = -1;
+
 void print_table(std::vector<std::vector<long long> > table)
 {
     for (int i = 0; i < table.size(); ++i)
@@ -20,63 +23,60 @@ void print_table(std::vector<std::vector<long long> > table)
     std::cout << '\n';
 }
 
-std::string restore_way_back(std::vector<std::vector<std::pair<long long,long long> > > way,
-                             long long sx, long long sy, std::string str)
+std::string restore_way_back(Way way, long long row, long long column, std::string str)
 {
-    std::string palindrome = "";
-    std::string middle = "";
-    while (sx != -1 && sy != -1)
-    {
-        long long new_x = way[sx][sy].first;
-        long long new_y = way[sx][sy].second;
+    std::string prefix;
+    std::string middle;
 
-        if ((new_x + 1 == sx && new_y - 1 == sy) ||
-            (new_x ==  -1 && new_y == -1))
+    while (row != WAY_END && column != WAY_END)
+    {
+        long long new_row = way[row][column].first;
+        long long new_col = way[row][column].second;
+
+        if ((new_row + 1 == row && new_col - 1 == column) ||
+            (new_row ==  WAY_END && new_col == WAY_END))
         {
-            if (sx != sy)
+            if (row != column)
             {
-                palindrome = palindrome + str[sx];
+                prefix = prefix + str[row];
             }
             else
             {
-                middle = str[sx];
+                middle = str[row];
             }
         }
 
-        sx = new_x;
-        sy = new_y;
+        row = new_row;
+        column = new_col;
     }
 
-    std::string reverse_str = palindrome;
-    std::reverse(reverse_str.begin(), reverse_str.end());
-    palindrome = palindrome + middle + reverse_str;
+    std::string suffix = prefix;
+    std::reverse(suffix.begin(), suffix.end());
 
-    return palindrome;
+    return prefix + middle + suffix;
 }
 
-std::string get_max_palindrome(std::string input)
+std::string get_max_palindrome(std::string input_str)
 {
-    long long len = input.size();
-    std::vector<std::vector<long long> > max_palindrome (len, std::vector<long long>(len, 0));
+    long long len = input_str.size();
+    std::vector<std::vector<long long> >
+            max_palindrome(len, std::vector<long long>(len, 0));
 
-    std::vector<std::vector<std::pair<long long,long long> > >
-            back_way (len, std::vector<std::pair<long long,long long> >(len, std::make_pair(0, 0)));
+    Way back_way(len, std::vector<std::pair<long long,long long> >
+                      (len, std::make_pair(0, 0)));
 
-    //print_table(max_palindrome);
     for (int i = 0; i < len; ++i)
     {
         max_palindrome[i][i] = 1;
-        back_way[i][i] = std::make_pair(-1, -1);
+        back_way[i][i] = std::make_pair(WAY_END, WAY_END);
     }
-
-    //print_table(max_palindrome);
 
     for (int j = 0; j + 1 < len; ++j)
     {
-        if (input[j] == input[j + 1])
+        if (input_str[j] == input_str[j + 1])
         {
             max_palindrome[1 + j][j] = 2;
-            back_way[1 + j][j] = std::make_pair(-1, -1);
+            back_way[1 + j][j] = std::make_pair(WAY_END, WAY_END);
         }
         else
         {
@@ -85,13 +85,11 @@ std::string get_max_palindrome(std::string input)
         }
     }
 
-    //print_table(max_palindrome);
-
     for (int i = 2; i < len; ++i)
     {
         for (int j = 0; j + i < len; ++j)
         {
-            if (input[i + j] == input[j])
+            if (input_str[i + j] == input_str[j])
             {
                 max_palindrome[i + j][j] = max_palindrome[i + j - 1][j + 1] + 2;
                 back_way[i + j][j] = std::make_pair(i + j - 1, j + 1);
@@ -100,27 +98,29 @@ std::string get_max_palindrome(std::string input)
             {
                 max_palindrome[i + j][j] = max_palindrome[i + j - 1][j];
                 back_way[i + j][j] = std::make_pair(i + j - 1, j);
+
                 if (max_palindrome[i + j][j] < max_palindrome[i + j][j + 1])
                 {
                     max_palindrome[i + j][j] = max_palindrome[i + j][j + 1];
                     back_way[i + j][j] = std::make_pair(i + j, j + 1);
                 }
             }
-            //print_table(max_palindrome);
         }
     }
 
-    //print_table(max_palindrome);
-    return restore_way_back(back_way, len - 1, 0, input);
+    return restore_way_back(back_way, len - 1, 0, input_str);
 }
 
 int main()
 {
+    std::ios_base::sync_with_stdio(false);
 
     std::string in_str;
     std::cin >> in_str;
+
     std::string answer = get_max_palindrome(in_str);
     std::cout << answer.size() << '\n' << answer << '\n';
+
     return 0;
 }
 
